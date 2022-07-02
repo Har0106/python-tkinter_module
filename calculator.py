3158c27bdfccb9b80763925c2fe87e668b6c875a
Har0106 <freddiemaron@gmail.com>
2022-07-02 13:21:20 +0530

    removed unwanted variables

--- calculator.py
+++ calculator.py
@@ -21,9 +21,9 @@ def disable(get):
         answer = eval(s)
     # to disable and enable equal button
     except:
-        button_equal = Button(root, text='=', font="Calibri 13", width=7, command=equal, state='disabled', bg='#808080').grid(row=6,column=3)
+        Button(root, text='=', font="Calibri 13", width=7, command=equal, state='disabled', bg='#808080').grid(row=6,column=3)
     else:
-        button_equal = Button(root, text='=', font="Calibri 13", width=7, command=equal, state='normal', bg='#808080').grid(row=6,column=3)
+        Button(root, text='=', font="Calibri 13", width=7, command=equal, state='normal', bg='#808080').grid(row=6,column=3)
 
 # to clear the scree of calculator
 def clear():
@@ -52,34 +52,34 @@ def equal():
     entry.insert(0, answer)
 
 # Design
-button_c = Button(root, text='C', font="Calibri 13", width=7, command=clear).grid(row=1,column=0)
-button_b = Button(root, text='B', font="Calibri 13", width=7, command=back).grid(row=1,column=1)
-button_bracket1 = Button(root, text='(', font="Calibri 13", width=7, command=lambda: chars('(')).grid(row=1,column=2)
-button_bracket2 = Button(root, text=')', font="Calibri 13", width=7, command=lambda: chars(')')).grid(row=1,column=3)
+Button(root, text='C', font="Calibri 13", width=7, command=clear).grid(row=1,column=0)
+Button(root, text='B', font="Calibri 13", width=7, command=back).grid(row=1,column=1)
+Button(root, text='(', font="Calibri 13", width=7, command=lambda: chars('(')).grid(row=1,column=2)
+Button(root, text=')', font="Calibri 13", width=7, command=lambda: chars(')')).grid(row=1,column=3)
 
-button_7 = Button(root, text='7', font="Calibri 13", width=7, command=lambda: chars('7')).grid(row=2,column=0)
-button_8 = Button(root, text='8', font="Calibri 13", width=7, command=lambda: chars('8')).grid(row=2,column=1)
-button_9 = Button(root, text='9', font="Calibri 13", width=7, command=lambda: chars('9')).grid(row=2,column=2)
-button_divide = Button(root, text='÷', font="Calibri 13", width=7, command=lambda: chars('÷')).grid(row=2,column=3)
+Button(root, text='7', font="Calibri 13", width=7, command=lambda: chars('7')).grid(row=2,column=0)
+Button(root, text='8', font="Calibri 13", width=7, command=lambda: chars('8')).grid(row=2,column=1)
+Button(root, text='9', font="Calibri 13", width=7, command=lambda: chars('9')).grid(row=2,column=2)
+Button(root, text='÷', font="Calibri 13", width=7, command=lambda: chars('÷')).grid(row=2,column=3)
 
-button_4 = Button(root, text='4', font="Calibri 13", width=7, command=lambda: chars('4')).grid(row=3,column=0)
-button_5 = Button(root, text='5', font="Calibri 13", width=7, command=lambda: chars('5')).grid(row=3,column=1)
-button_6 = Button(root, text='6', font="Calibri 13", width=7, command=lambda: chars('6')).grid(row=3,column=2)
-button_multiply = Button(root, text='x', font="Calibri 13", width=7, command=lambda: chars('x')).grid(row=3,column=3)
+Button(root, text='4', font="Calibri 13", width=7, command=lambda: chars('4')).grid(row=3,column=0)
+Button(root, text='5', font="Calibri 13", width=7, command=lambda: chars('5')).grid(row=3,column=1)
+Button(root, text='6', font="Calibri 13", width=7, command=lambda: chars('6')).grid(row=3,column=2)
+Button(root, text='x', font="Calibri 13", width=7, command=lambda: chars('x')).grid(row=3,column=3)
 
-button_1 = Button(root, text='1', font="Calibri 13", width=7, command=lambda: chars('1')).grid(row=4,column=0)
-button_2 = Button(root, text='2', font="Calibri 13", width=7, command=lambda: chars('2')).grid(row=4,column=1)
-button_3 = Button(root, text='3', font="Calibri 13", width=7, command=lambda: chars('3')).grid(row=4,column=2)
-button_subtract = Button(root, text='-', font="Calibri 13", width=7, command=lambda: chars('-')).grid(row=4,column=3)
+Button(root, text='1', font="Calibri 13", width=7, command=lambda: chars('1')).grid(row=4,column=0)
+Button(root, text='2', font="Calibri 13", width=7, command=lambda: chars('2')).grid(row=4,column=1)
+Button(root, text='3', font="Calibri 13", width=7, command=lambda: chars('3')).grid(row=4,column=2)
+Button(root, text='-', font="Calibri 13", width=7, command=lambda: chars('-')).grid(row=4,column=3)
 
-button_power = Button(root, text='x²', font="Calibri 13", width=7, command=lambda: chars('^2')).grid(row=5,column=0)
-button_0 = Button(root, text='0', font="Calibri 13", width=7, command=lambda: chars('0')).grid(row=5,column=1)
-button_decimal = Button(root, text='.', font="Calibri 13", width=7, command=lambda: chars('.')).grid(row=5,column=2)
-button_add = Button(root, text='+', font="Calibri 13", width=7, command=lambda: chars('+')).grid(row=5,column=3)
+Button(root, text='x²', font="Calibri 13", width=7, command=lambda: chars('^2')).grid(row=5,column=0)
+Button(root, text='0', font="Calibri 13", width=7, command=lambda: chars('0')).grid(row=5,column=1)
+Button(root, text='.', font="Calibri 13", width=7, command=lambda: chars('.')).grid(row=5,column=2)
+Button(root, text='+', font="Calibri 13", width=7, command=lambda: chars('+')).grid(row=5,column=3)
 
-button_squareroot = Button(root, text='√', font="Calibri 13", width=7, command=lambda: chars('√(')).grid(row=6,column=0)
-button_divdeby1 = Button(root, text='1/x', font="Calibri 13", width=7, command=lambda: chars('(1/')).grid(row=6,column=1)
-button_percent = Button(root, text='%', font="Calibri 13", width=7, command=lambda: chars('%')).grid(row=6,column=2)
-button_equal = Button(root, text='=', font="Calibri 13", width=7, command=equal, state='disabled', bg='#808080').grid(row=6,column=3)
+Button(root, text='√', font="Calibri 13", width=7, command=lambda: chars('√(')).grid(row=6,column=0)
+Button(root, text='1/x', font="Calibri 13", width=7, command=lambda: chars('(1/')).grid(row=6,column=1)
+Button(root, text='%', font="Calibri 13", width=7, command=lambda: chars('%')).grid(row=6,column=2)
+Button(root, text='=', font="Calibri 13", width=7, command=equal, state='disabled', bg='#808080').grid(row=6,column=3)
 
 root.mainloop()
