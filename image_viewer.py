193ab15e9357041d540ea519954a98d2384971af
Har0106 <freddiemaron@gmail.com>
2022-07-02 13:23:46 +0530

    removed unwanted variables

--- image_viewer.py
+++ image_viewer.py
@@ -17,16 +17,15 @@ image.grid(columnspan=3, pady=8, padx=8, row=0, column=0)
 def forward(number):
     global image
     
-    button_backward = Button(root, text='<', width=4, command=lambda:backward(number-1), state='normal').grid(row=1, column=0, pady=5)
-
     image.grid_forget()
     image = Label(image=images[number+1])
     image.grid(columnspan=3, pady=8, padx=8, row=0, column=0)
+    Button(root, text='<', width=4, command=lambda:backward(number-1), state='normal').grid(row=1, column=0, pady=5)
 
     if number + 1 == 4:
-        button_forward = Button(root, text='>', width=4, state='disabled').grid(row=1, column=2, pady=5)
+        Button(root, text='>', width=4, state='disabled').grid(row=1, column=2, pady=5)
         return
-    button_forward = Button(root, text='>', width=4, command=lambda:forward(number+1)).grid(row=1, column=2, pady=5)
+    Button(root, text='>', width=4, command=lambda:forward(number+1)).grid(row=1, column=2, pady=5)
 
 def backward(number):
     global image
@@ -34,16 +33,15 @@ def backward(number):
     image.grid_forget()
     image = Label(image=images[number+1])
     image.grid(columnspan=3, pady=8, padx=8, row=0, column=0)
-
-    button_forward = Button(root, text='>', width=4, command=lambda:forward(number+1)).grid(row=1, column=2, pady=5)
+    Button(root, text='>', width=4, command=lambda:forward(number+1)).grid(row=1, column=2, pady=5)
 
     if number+1 == 0:
-        button_backward = Button(root, text='<', width=4, state='disabled').grid(row=1, column=0, pady=5)
+        Button(root, text='<', width=4, state='disabled').grid(row=1, column=0, pady=5)
         return
-    button_backward = Button(root, text='<', width=4, command=lambda:backward(number-1)).grid(row=1, column=0, pady=5)
+    Button(root, text='<', width=4, command=lambda:backward(number-1)).grid(row=1, column=0, pady=5)
 
-button_backward = Button(root, text='<', width=4, state='disabled').grid(row=1, column=0, pady=5)
-button_close = Button(root, text='Close', width=5, command=root.quit).grid(row=1, column=1, pady=5)
-button_forward = Button(root, text='>', width=4, command=lambda:forward(0)).grid(row=1, column=2, pady=5)
+Button(root, text='<', width=4, state='disabled').grid(row=1, column=0, pady=5)
+Button(root, text='Close', width=5, command=root.quit).grid(row=1, column=1, pady=5)
+Button(root, text='>', width=4, command=lambda:forward(0)).grid(row=1, column=2, pady=5)
 
 root.mainloop()
