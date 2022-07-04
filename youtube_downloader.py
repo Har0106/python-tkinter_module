4cdbc5d26b9c40b753b94688e9011e397d3efa1d
Har0106 <freddiemaron@gmail.com>
2022-07-04 12:38:34 +0530

    added download and download next

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,14 +1,29 @@
 from tkinter import *
+from pytube import YouTube
+from tkinter import messagebox
 
 root = Tk()
-root.title('Youtube Downloader')
+root.title('YouTube Downloader')
 
 def download():
-    pass
+    try:
+        y = YouTube(entry.get())
+        video = y.streams.get_highest_resolution()
+        try:
+            video.download()
+        except:
+            messagebox.showerror('showerror', 'Some error occured')
+    except:
+        messagebox.showinfo('showinfo', 'Check your network connection or check the URL')
 
-Label(root, text='YouTube Downloader', font='Arial 25').grid(row=0, column=0, pady=10)
-entry = Entry(root, width=30, font='Arial 15')
-entry.grid(row=1, column=0, padx=75, pady=10)
-Button(root, text='Download', font='Arial 12').grid(row=2, column=0, pady=10)
+def download_next():
+    entry.delete(0, END)
+
+Label(root, text='YouTube Downloader', font='Arial 25').grid(row=0, column=0, pady=10, columnspan=2)
+Label(root, text='Enter the URL', font='Arial 15').grid(row=1, column=0, columnspan=2)
+entry = Entry(root, width=35, font='Arial 15')
+entry.grid(row=2, column=0, padx=50, pady=10, columnspan=2)
+Button(root, text='Download', font='Arial 12', command=download).grid(row=3, column=0, pady=10, padx=5, sticky='e')
+Button(root, text='Download Next', font='Arial 12', command=download_next).grid(row=3, column=1, pady=10, padx=5, sticky='w')
 
 root.mainloop()
