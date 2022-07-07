045f73bc916182d1aa7b9ca1122a5ae6d4bf7fe3
Har0106 <freddiemaron@gmail.com>
2022-07-07 14:04:09 +0530

    changed to design

--- geography_flashcard_app.py
+++ geography_flashcard_app.py
@@ -4,7 +4,6 @@ import random
 
 root = Tk()
 root.title('Geography Flash Card')
-root.geometry('525x525')
 
 def hide_frames():
     for widget in state_frame.winfo_children():
@@ -18,10 +17,10 @@ def states():
     hide_frames()
     state_frame.pack(fill='both', expand=1)
     global image
-    states = ['alaska', 'arizona', 'california', 'colorado', 'florida', 'hawaii', 'massachusetts', 'michigan', 'montana', 'new_jersey', 'ohio', 'pennsylvania', 'texas', 'virginia', 'washington']
-    state = f'states/{random.choice(states)}.jpg'
-    image = ImageTk.PhotoImage(Image.open(state).resize((250, 250)))
-    Label(state_frame, image=image).pack(pady=20)
+    states = ['alaska', 'arizona', 'california', 'colorado', 'florida', 'hawaii', 'massachusetts', 'michigan', 'montana', 'newjersey', 'ohio', 'pennsylvania', 'texas', 'virginia', 'washington']
+    state = random.choice(states)
+    image = ImageTk.PhotoImage(Image.open(f'states/{state}.jpg').resize((250, 250)))
+    Label(state_frame, image=image).pack(pady=20, padx=100)
 
 def state_capitals():
     hide_frames()
@@ -35,7 +34,7 @@ menu.add_cascade(label='State', menu=state)
 state.add_command(label='States', command=states)
 state.add_command(label='State Capitals', command=state_capitals)
 
-state_frame = Frame(root, width=525, height=525)
-state_capitals_frame = Frame(root, width=525, height=525)
+state_frame = Frame(root)
+state_capitals_frame = Frame(root)
 
 root.mainloop()
