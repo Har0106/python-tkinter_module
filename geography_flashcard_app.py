090f65beb9ee17b669e6a45341979e7c943da116
Har0106 <freddiemaron@gmail.com>
2022-07-07 19:06:13 +0530

    correct or incorrect

--- geography_flashcard_app.py
+++ geography_flashcard_app.py
@@ -5,6 +5,14 @@ import random
 root = Tk()
 root.title('Geography Flash Card')
 
+def answer():
+    answer = state.replace('_', ' ').title()
+    if answer == entry.get().title():
+        text = 'Correct!'
+    else:
+        text = f'Incorrect! And The Correct Answer is {answer}'
+    label.configure(text=text)
+
 def hide_frames():
     for widget in state_frame.winfo_children():
         widget.destroy()
@@ -16,11 +24,22 @@ def hide_frames():
 def states():
     hide_frames()
     state_frame.pack(fill='both', expand=1)
+    global state
     global image
-    states = ['alaska', 'arizona', 'california', 'colorado', 'florida', 'hawaii', 'massachusetts', 'michigan', 'montana', 'newjersey', 'ohio', 'pennsylvania', 'texas', 'virginia', 'washington']
+    states = ['alaska', 'arizona', 'california', 'colorado', 'florida', 'hawaii', 'massachusetts', 'michigan', 'montana', 'new_jersey', 'ohio', 'pennsylvania', 'texas', 'virginia', 'washington']
     state = random.choice(states)
     image = ImageTk.PhotoImage(Image.open(f'states/{state}.jpg').resize((250, 250)))
-    Label(state_frame, image=image).pack(pady=20, padx=100)
+    Label(state_frame, image=image).pack(pady=20, padx=150)
+
+    global entry
+    entry = Entry(state_frame, font='Arial 15')
+    entry.pack(pady=10)
+
+    Button(state_frame, text='Submit', font='Arial 13', command=answer).pack(pady=10)
+
+    global label
+    label = Label(state_frame, text='', font='Arial 15', width=50)
+    label.pack(pady=10)
 
 def state_capitals():
     hide_frames()
