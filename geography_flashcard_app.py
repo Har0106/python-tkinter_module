b8b23daff2e17b555d11abce4057e9343606def8
Har0106 <freddiemaron@gmail.com>
2022-07-07 13:38:18 +0530

    showing a image on screen

--- geography_flashcard_app.py
+++ geography_flashcard_app.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import random
 
 root = Tk()
 root.title('Geography Flash Card')
-root.geometry('550x550')
+root.geometry('525x525')
 
 def hide_frames():
     for widget in state_frame.winfo_children():
@@ -16,6 +17,11 @@ def hide_frames():
 def states():
     hide_frames()
     state_frame.pack(fill='both', expand=1)
+    global image
+    states = ['alaska', 'arizona', 'california', 'colorado', 'florida', 'hawaii', 'massachusetts', 'michigan', 'montana', 'new_jersey', 'ohio', 'pennsylvania', 'texas', 'virginia', 'washington']
+    state = f'states/{random.choice(states)}.jpg'
+    image = ImageTk.PhotoImage(Image.open(state).resize((250, 250)))
+    Label(state_frame, image=image).pack(pady=20)
 
 def state_capitals():
     hide_frames()
@@ -29,7 +35,7 @@ menu.add_cascade(label='State', menu=state)
 state.add_command(label='States', command=states)
 state.add_command(label='State Capitals', command=state_capitals)
 
-state_frame = Frame(root, width=550, height=550)
-state_capitals_frame = Frame(root, width=550, height=550)
+state_frame = Frame(root, width=525, height=525)
+state_capitals_frame = Frame(root, width=525, height=525)
 
 root.mainloop()
