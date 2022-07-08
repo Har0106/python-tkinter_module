cd4adb18e765df50839c6c9ae6ef700e382216f3
Har0106 <freddiemaron@gmail.com>
2022-07-08 14:47:09 +0530

    chack answer

--- math_flashcard_app.py
+++ math_flashcard_app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk, Image
 import random
 
@@ -21,36 +22,49 @@ def hide_frames():
         widget.destroy()
     for widget in multiplication_frame.winfo_children():
         widget.destroy()
-    for widget in division_frame.winfo_children():
-        widget.destroy()
 
     home_numbers.pack_forget()
     home_label.pack_forget()
     additon_frame.pack_forget()
     subtraction_frame.pack_forget()
     multiplication_frame.pack_forget()
-    division_frame.pack_forget()
 
 def home():
     hide_frames()
     home_numbers.pack()
     home_label.pack(pady=20)
 
+def answers(symbol):
+    if symbol == 'x':
+        symbol = '*'
+    try:
+        if eval(str(num1) + symbol + str(num2)) == int(entry.get()):
+            text = 'Correct!'
+        else:
+            text = 'Incorrect!'
+        op_label.configure(text=text)
+    except:
+        messagebox.showerror('Error', 'Invalid Number Entered')
+
 def operations(operation, frame, func):
+    global num1
+    global num2
     num1 = random.randint(0, 9)
     num2 = random.randint(0, 9)
     Label(frame, text=num1, font='Arial 150').grid(row=0, column=0, padx=(50, 10))
     Label(frame, text=operation, font='Arial 100').grid(row=0, column=1, padx=10)
     Label(frame, text=num2, font='Arial 150').grid(row=0, column=2, padx=(10, 50))
 
+    global entry
     entry = Entry(frame, font='Arial 25', justify='center', width=10)
     entry.grid(row=1, column=0, columnspan=3, pady=(0, 30))
 
-    Button(frame, font='Arial 13', text='Submit').grid(padx=10, pady=(0, 20), row=2, column=0, sticky='e')
+    Button(frame, font='Arial 13', text='Submit', command=lambda: answers(operation)).grid(padx=10, pady=(0, 20), row=2, column=0, sticky='e')
     Button(frame, font='Arial 13', text='Next', command=func).grid(padx=10, pady=(0, 20), row=2, column=2, sticky='w')
 
+    global op_label
     op_label = Label(frame, font='Arial 15', text='')
-    op_label.grid(pady=10)
+    op_label.grid(pady=(10, 20), columnspan=3)
 
 def additon():
     hide_frames()
@@ -67,12 +81,6 @@ def multiplication():
     multiplication_frame.pack(fill='both', expand=1)
     operations('x', multiplication_frame, multiplication)
 
-def division():
-    hide_frames()
-    division_frame.pack(fill='both', expand=1)
-    operations('÷', division_frame, division)
-    Label(division_frame, text='Give The Answer\nIn Rounded Form', font='Arial 15', justify='center').grid(row=3, column=0, columnspan=3, pady=10)
-
 menu = Menu(root)
 root.config(menu=menu)
 
@@ -86,11 +94,9 @@ menu.add_cascade(label='Math', menu=math)
 math.add_command(label='Addition', command=additon)
 math.add_command(label='Subtraction', command=subtraction)
 math.add_command(label='Multiplication', command=multiplication)
-math.add_command(label='Division', command=division)
 
 additon_frame = Frame(root)
 subtraction_frame = Frame(root)
 multiplication_frame = Frame(root)
-division_frame = Frame(root)
 
 root.mainloop()
