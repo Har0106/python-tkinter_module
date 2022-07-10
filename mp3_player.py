03709155f59b524af1d12d947560236f4e95bdac
Har0106 <freddiemaron@gmail.com>
2022-07-10 20:35:20 +0530

    forward and backward fuction

--- mp3_player.py
+++ mp3_player.py
@@ -1,10 +1,23 @@
 from tkinter import *
 from tkinter import filedialog
-import pygame
+from pygame import mixer
 
 root = Tk()
 root.title('MP3 Player')
-pygame.mixer.init()
+mixer.init()
+
+global sound
+sound = None
+
+def disabel_normal():
+    if listboxes.get(0, END)[-1] == listboxes.get(0, END)[sel]:
+       forward_button.configure(state='disabled')
+    else:
+       forward_button.configure(state='normal')
+    if listboxes.get(0, END)[0] == listboxes.get(0, END)[sel]:
+       backward_button.configure(state='disabled')
+    else:
+       backward_button.configure(state='normal')
 
 def add_song():
     global songs
@@ -14,36 +27,51 @@ def add_song():
         listboxes.insert(END, a)
 
 def play_pause():
-        if mid.cget('text') == u"\u25B6":
-            mid.configure(text=u'\u23F8')
-        else:
-            mid.configure(text=u"\u25B6")
+    if mid.cget('text') == u"\u25B6":
+        mid.configure(text=u'\u23F8')
+        mixer.pause()
+    else:
+        mid.configure(text=u"\u25B6")
+        mixer.unpause()
 
-global sound
-sound = ''
+def play_sound():
+    global sound
+    try:
+        sound.stop()
+    except:
+        pass
+    for i,a in songs:
+        if listboxes.get(0, END)[sel] == a:
+            sound = mixer.Sound(i)
+            sound.play()
 
-def play_sound(event):
-    sel = listboxes.curselection()
-    if sel:
-        global sound
-        try:
-            sound.stop()
-        except:
-            pass
-        for i,a in songs:
-            if listboxes.get(0, END)[sel[0]] == a:
-                print(i)
-                sound = pygame.mixer.Sound(i)
-                sound.play()
+def get_sound(event):
+    global sel
+    sel = listboxes.curselection()[0]
+    disabel_normal()
+    play_sound()
 
-listboxes = Listbox(root, width=40, font='Arial 15')
+def forward_backward(v):
+    global sel
+    if v:
+        sel = listboxes.curselection()[0] + 1
+    else:
+        sel = listboxes.curselection()[0] - 1
+    listboxes.selection_clear(listboxes.curselection()[0])
+    listboxes.selection_set(sel)
+    disabel_normal()
+    play_sound()
+
+listboxes = Listbox(root, width=40, font='Arial 15', activestyle='none')
 listboxes.grid(row=0, column=0, columnspan=3, padx=25, pady=(20, 15))
-root.bind('<Button-1>', play_sound)
+listboxes.bind('<<ListboxSelect>>', get_sound)
 
-Button(root, text=u'\u23EE', font='Arial 20', bd=0).grid(row=1, column=0, pady=(0, 10), sticky='e')
+backward_button = Button(root, text=u'\u23EE', font='Arial 20', bd=0, command=lambda: forward_backward(0))
+backward_button.grid(row=1, column=0, pady=(0, 10), sticky='e')
 mid = Button(root, text=u"\u25B6", font='Arial 20', bd=0, command=play_pause)
 mid.grid(row=1, column=1, pady=(0, 10))
-Button(root, text=u'\u23ED', font='Arial 20', bd=0).grid(row=1, column=2, pady=(0, 10), sticky='w')
+forward_button = Button(root, text=u'\u23ED', font='Arial 20', bd=0, command=lambda: forward_backward(1))
+forward_button.grid(row=1, column=2, pady=(0, 10), sticky='w')
 
 menu = Menu(root)
 root.config(menu=menu)
