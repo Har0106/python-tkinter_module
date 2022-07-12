2876da6da8735862ee4143df182db7e95ae9e64f
Har0106 <freddiemaron@gmail.com>
2022-07-12 21:47:24 +0530

    remove test files

--- test1.py
+++
@@ -1,5 +0,0 @@
-print('Hello World')
-print('I am making a notepad')
-
-for i in range(10):
-    print(i)
--- text2.py
+++
@@ -1,8 +0,0 @@
-print('This is the second text file')
-print('This is the second text file')
-print('This is the second text file')
-print('This is the second text file')
-
-
-
-
