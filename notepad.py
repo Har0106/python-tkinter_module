9e3f7e0145916540c4659c028c28d209a1c08f40
Har0106 <freddiemaron@gmail.com>
2022-07-16 16:04:04 +0530

    added comments to the project

--- notepad.py
+++ notepad.py
@@ -2,17 +2,20 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 
+# Creating the gui window
 root = Tk()
 root.title('Untitled - Notepad')
 
 cut_copy = None
 file_name = None
 
+# Opening a new file
 def new(event):
     text.delete(1.0, END)
     root.title('Untitled - Notepad')
     status_bar.configure(text='Unsaved    ')
 
+# Opening an existing file
 def open_file(event):
     global file_name
     file_name = filedialog.askopenfilename(filetypes=(('Python Files', '*.py'), ('Text Files', '*.txt'), ('All Files', '*.*')))
@@ -24,6 +27,7 @@ def open_file(event):
             text.insert(END, file.read())
         status_bar.configure(text='Saved    ')
 
+# Save the file with a new name
 def save_as(event):
     global file_name
     file_name = filedialog.asksaveasfilename(filetypes=(('Python Files', '*.py'), ('Text Files', '*.txt'), ('All Files', '*.*')))
@@ -34,6 +38,7 @@ def save_as(event):
             file.write(text.get(1.0, END)[:-1])
         status_bar.configure(text='Saved    ')
 
+# Save the changes made to the opened file
 def save(event):
     if root.title() == 'Untitled - Notepad':
         save_as(None)
@@ -42,6 +47,7 @@ def save(event):
             file.write(text.get(1.0, END)[:-1])
     status_bar.configure(text='Saved    ')
 
+# Cut the selected text
 def cut(event):
     global cut_copy
     if event:
@@ -52,6 +58,7 @@ def cut(event):
         root.clipboard_append(cut_copy)
         text.delete('sel.first', 'sel.last')
 
+# Copy the selected text to the clipboard
 def copy(event):
     global cut_copy
     if event:
@@ -61,6 +68,7 @@ def copy(event):
         root.clipboard_clear()
         root.clipboard_append(cut_copy)
 
+# Paste the text in the clipboard whereever the curser is
 def paste(event):
     global cut_copy
     if event:
@@ -69,9 +77,11 @@ def paste(event):
         pos = text.index(INSERT)
         text.insert(pos, cut_copy)
 
+# Select all the text in the notepad
 def select_all():
     text.tag_add('sel', '1.0', 'end')
 
+# Change white widgets to black
 def dark_mode(event):
     text.configure(bg='#1f1d1d', fg='white', insertbackground='white', selectbackground='white', selectforeground='black')
     file_menu.configure(bg='#1f1d1d', fg='white')
@@ -80,6 +90,7 @@ def dark_mode(event):
     view_menu.configure(bg='#1f1d1d', fg='white')
     help_menu.configure(bg='#1f1d1d', fg='white')
 
+# Change black widgets to white
 def light_mode(event):
     text.configure(bg='white', fg='black', insertbackground='black', selectbackground='black', selectforeground='white')
     file_menu.configure(bg='SystemButtonFace', fg='black')
@@ -88,9 +99,7 @@ def light_mode(event):
     view_menu.configure(bg='SystemButtonFace', fg='black')
     help_menu.configure(bg='SystemButtonFace', fg='black')
 
-def about():
-    messagebox.showinfo('About', 'Notepad by Har0106')
-
+# Updating the status bar as the user types something in
 def refresh_status():
     if file_name:
         with open(file_name, 'r') as file:
@@ -100,9 +109,13 @@ def refresh_status():
                 status_bar.configure(text='Unsaved    ')
     root.after(1000, refresh_status)
 
+# Design of the notepad
+
+# Main frame of the notepad
 frame = Frame(root)
 frame.pack()
 
+# Horizontal and vertical scrollbar for the text widget
 text_scroll_y = Scrollbar(frame)
 text_scroll_x = Scrollbar(frame, orient='horizontal')
 text_scroll_y.pack(fill='y', side='right')
@@ -112,12 +125,15 @@ text.pack()
 text_scroll_y.configure(command=text.yview) 
 text_scroll_x.configure(command=text.xview)
 
+# Status bar
 status_bar = Label(root, text='Unsaved    ', relief='groove', anchor='e', bg='#808080')
 status_bar.pack(fill='x', side='bottom')
 
+# Main menu of the notepad
 menu = Menu(frame)
 root.configure(menu=menu)
 
+# To create new file, open an existing file, save a file in another name and save the changes to a file 
 file_menu = Menu(menu, tearoff=False, fg='black')
 menu.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label='New', accelerator='Ctrl+N', command=lambda: new(None))
@@ -132,6 +148,7 @@ root.bind('<Control-Shift-S>', save_as)
 file_menu.add_separator()
 file_menu.add_command(label='Exit', command=root.destroy)
 
+# Cut, copy and paste text and redo or undo the changes made to a file
 edit_menu = Menu(menu, tearoff=False, fg='black')
 menu.add_cascade(label='Edit', menu=edit_menu)
 edit_menu.add_command(label='Undo       ', accelerator='Ctrl+Z')
@@ -144,10 +161,12 @@ root.bind('<Control-c>', copy)
 edit_menu.add_command(label='Paste', accelerator='Ctrl+V', command=lambda: paste(None))
 root.bind('<Control-v>', paste)
 
+# Select all the text from the notepad
 selection_menu = Menu(menu, tearoff=False, fg='black')
 menu.add_cascade(label='Selection', menu=selection_menu)
 selection_menu.add_command(label='Select All       ', accelerator='Ctrl+A', command=select_all)
 
+# Switch from light mode to dark mode and vice versa
 view_menu = Menu(menu, tearoff=False, fg='black')
 menu.add_cascade(label='View', menu=view_menu)
 view_menu.add_command(label='Dark Mode       ', accelerator='Ctrl+Alt+D', command=lambda: dark_mode(None))
@@ -155,9 +174,10 @@ root.bind('<Control-Alt-d>', dark_mode)
 view_menu.add_command(label='Light Mode       ', accelerator='Ctrl+Alt+L', command=lambda: light_mode(None))
 root.bind('<Control-Alt-l>', light_mode)
 
+# About the notepad
 help_menu = Menu(menu, tearoff=False, fg='black')
 menu.add_cascade(label='Help', menu=help_menu)
-help_menu.add_command(label='About       ', command=about)
+help_menu.add_command(label='About       ', command=lambda:messagebox.showinfo('About', 'Notepad by Har0106'))
 
 refresh_status()
 root.mainloop()
