4be4350deb79622e11a0910fea50b66bcb7d3044
Har0106 <freddiemaron@gmail.com>
2022-07-17 14:01:07 +0530

    adding comments to the project

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -2,28 +2,36 @@ from tkinter import *
 from pytube import YouTube
 from tkinter import messagebox
 
+# main gui window of the app
 root = Tk()
 root.title('YouTube Downloader')
 
+# getting the link and downloading it
 def download():
     try:
         y = YouTube(entry.get())
         video = y.streams.get_highest_resolution()
         try:
             video.download()
+            label.configure(text='Downloaded!')
         except:
             messagebox.showerror('showerror', 'Some error occured')
     except:
         messagebox.showinfo('showinfo', 'Check your network connection or check the URL')
 
+# deleting the link in the entry widget
 def download_next():
     entry.delete(0, END)
+    label.configure(text='')
 
+# design of the app
 Label(root, text='YouTube Downloader', font='Arial 25').grid(row=0, column=0, pady=10, columnspan=2)
 Label(root, text='Enter the URL', font='Arial 15').grid(row=1, column=0, columnspan=2)
 entry = Entry(root, width=35, font='Arial 15')
 entry.grid(row=2, column=0, padx=50, pady=10, columnspan=2)
-Button(root, text='Download', font='Arial 12', command=download).grid(row=3, column=0, pady=10, padx=5, sticky='e')
-Button(root, text='Download Next', font='Arial 12', command=download_next).grid(row=3, column=1, pady=10, padx=5, sticky='w')
+Button(root, text='Download', font='Arial 12', command=download).grid(row=3, column=0, pady=5, padx=5, sticky='e')
+Button(root, text='Download Next', font='Arial 12', command=download_next).grid(row=3, column=1, pady=5, padx=5, sticky='w')
+label = Label(root, text='', font='Arial 14')
+label.grid(row=4, column=0, pady=(5,10), columnspan=2)
 
 root.mainloop()
