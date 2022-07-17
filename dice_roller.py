804cc4dcd062f423e3dd22afe7dab4b480d0cab9
Har0106 <freddiemaron@gmail.com>
2022-07-17 13:31:31 +0530

    adding comments to the project

--- dice_roller.py
+++ dice_roller.py
@@ -1,15 +1,18 @@
 from tkinter import *
 import random
 
+# creating the main gui of the project
 root = Tk()
 root.title('Dice Roller')
 
+# choosing a random die from the list of dice and showing it on the screen
 def roll():
     dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
     die = random.choice(dice)
     label.configure(text=die)
     Label(root, text=f'You Rolled {dice.index(die) + 1}', font='Arial 15').grid(row=3, column=0, pady=30)
 
+# deign of the app
 Label(root, text='Dice Roller',font='Arial 30').grid(row=0, column=0, padx=75, pady=(20, 0))
 label = Label(root, text='\u2680', font='Arial 125')
 label.grid(row=1, column=0)
--- digital_clock.py
+++ digital_clock.py
@@ -1,15 +1,18 @@
 from tkinter import *
 import time
 
+# creating the main gui wondow of the project
 root = Tk()
 root.title('Digital Clock')
 
+# getting the current time and day of the week
 def clock():
     t = time.strftime('%H:%M:%S')
     label.config(text=t)
     label2.config(text=time.strftime('%A'))
     label.after(1000, clock)
 
+# design of the app
 label = Label(root, font='Arial 50', fg='white', bg='black')
 label2 = Label(root, font='Arial 30')
 label.grid(row=0, column=0)
