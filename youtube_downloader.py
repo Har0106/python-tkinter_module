c09df0437dad5315f206a4c3fd7ad3d69e464dba
Har0106 <freddiemaron@gmail.com>
2022-07-20 14:06:09 +0530

    adding video destination option

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -12,12 +12,12 @@ def download():
         y = YouTube(entry.get())
         video = y.streams.get_highest_resolution()
         try:
-            video.download()
+            video.download(path.get())
             label.configure(text='Downloaded!')
         except:
-            messagebox.showerror('showerror', 'Some error occured')
+            messagebox.showerror('Error', "Couldn't download the video")
     except:
-        messagebox.showinfo('showinfo', 'Check your network connection or check the URL')
+        messagebox.showinfo('Error', "Couldn't download the video")
 
 # deleting the link in the entry widget
 def download_next():
@@ -26,12 +26,19 @@ def download_next():
 
 # design of the app
 Label(root, text='YouTube Downloader', font='Arial 25').grid(row=0, column=0, pady=10, columnspan=2)
+
 Label(root, text='Enter the URL', font='Arial 15').grid(row=1, column=0, columnspan=2)
 entry = Entry(root, width=35, font='Arial 15')
 entry.grid(row=2, column=0, padx=50, pady=10, columnspan=2)
-Button(root, text='Download', font='Arial 12', command=download).grid(row=3, column=0, pady=5, padx=5, sticky='e')
-Button(root, text='Download Next', font='Arial 12', command=download_next).grid(row=3, column=1, pady=5, padx=5, sticky='w')
+
+Label(root, text='Enter the destination', font='Arial 15').grid(row=3, column=0, columnspan=2)
+path = Entry(root, width=35, font='Arial 15')
+path.grid(row=4, column=0, padx=50, pady=10, columnspan=2)
+
+Button(root, text='Download', font='Arial 12', command=download).grid(row=5, column=0, pady=5, padx=5, sticky='e')
+Button(root, text='Download Next', font='Arial 12', command=download_next).grid(row=5, column=1, pady=5, padx=5, sticky='w')
+
 label = Label(root, text='', font='Arial 14')
-label.grid(row=4, column=0, pady=(5,10), columnspan=2)
+label.grid(row=6, column=0, pady=(5,10), columnspan=2)
 
 root.mainloop()
